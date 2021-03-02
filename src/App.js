dadd8aae1034146f1a1d76c3ff7e6a987b33f0e4
carlericholm <ceholm@emisys.eu>
2021-03-02 17:34:37 +0100

    addind header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from '../src/pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
